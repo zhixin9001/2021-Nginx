df369a282a39268e9c18b9df98e81680f90fd668
zhi xin <zhi.xin@myob.com>
2021-11-18 13:01:37 +0800

    11.18

--- ops/nginx.sh
+++ ops/nginx.sh
@@ -2,6 +2,8 @@
 
 docker run --name nginx-demo --rm \
 -p 80:80 \
+-p 81:81 \
+-p 82:82 \
 -v "$(pwd)"/nginx/www:/usr/share/nginx/html \
 -v "$(pwd)"/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
 -v "$(pwd)"/nginx/log:/var/log/nginx  \
